c9870b272a460a9b34e8c5a2842bdda7e146b2db
Salman <salman9915189734@gmail.com>
2022-04-11 18:47:57 +0530

    News Page added

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -18,11 +18,12 @@ const Cryptocurrencies = ({ simplified }) => {
 
     if (isFetching) return 'Loading';
     return (
-        <>  {!simplified &&
-            <div className="search-crypto">
-                <Input placeholder='Search Crypto currency' onChange={(e) => setSearchTerm(e.target.value)} />
-            </div>
-        }
+        <>
+            {!simplified &&
+                <div className="search-crypto">
+                    <Input placeholder='Search Crypto currency' onChange={(e) => setSearchTerm(e.target.value)} />
+                </div>
+            }
 
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
